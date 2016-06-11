65483aaa92ea83699fbced3cbb2289ad07fb96bc
k3rn3l16 <k3rn3l16@outlook.com>
2016-06-11 00:53:19 -0600

    second run alongside md draft

--- proj1_rCodes.R
+++ proj1_rCodes.R
@@ -1,5 +1,11 @@
-# load data
-org.df <- read.csv("activity.csv", stringsAsFactors = FALSE)
+# download the relevant dataset from an URL source
+URL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
+temp <- tempfile()
+download.file(URL, temp)
+
+# load data and remove temp file
+org.df <- read.csv(unz(temp, "activity.csv"), stringsAsFactors = FALSE)
+unlink(temp)
 
 # explore dataset
 dim(org.df)
@@ -12,8 +18,10 @@ agg_date.df <- setNames(aggregate(org.df$steps ~ org.df$date, data = org.df, FUN
 head(agg_date.df)
 
 # mean and median steps per day
-mean(agg_date.df$Steps)
-median(agg_date.df$Steps)
+mean_org <- mean(agg_date.df$Steps)
+median_org <- median(agg_date.df$Steps)
+print(mean_org)
+print(median_org)
 
 # histogram of steps per day
 library(ggplot2)
@@ -27,26 +35,30 @@ ggplot(agg_int.df, aes(x = Interval, y = Steps)) +
         geom_line(color = "blue") +
         labs(title = "Time Series of Daily Average Steps over Intervals", x = "Intervals", y = "Number of Steps")
 # 5 minute interval with the maximum number of steps
-agg_int.df[which.max(agg_int.df$Steps),]
+max_int_avg <- agg_int.df[which.max(agg_int.df$Steps),]
+print(max_int_avg)
 
 # number of missing values
-sapply(org.df, function(x) sum(is.na(x)))
+missing_val <- sapply(org.df, function(x) sum(is.na(x)))
+print(missing_val)
 
 # imputing missing values
 # copy original dataset
 imp.df <- org.df
 # impute missing values with the mean
 imp.df$steps[is.na(imp.df$steps)] <- mean(imp.df$steps, na.rm = TRUE)
-sapply(imp.df, function(x) sum(is.na(x)))
+str(imp.df)
 # aggregate the imputed data set to get the total number of steps taken daily
 agg_date_imp.df <- setNames(aggregate(imp.df$steps ~ imp.df$date, data = imp.df, FUN = sum), c("Date","Steps"))
-# histogram of 
+# histogram of total number of steps taken each day (with imputed values)
 ggplot(agg_date_imp.df, aes(Steps)) +
         geom_histogram(binwidth = 2000, fill = "blue") +
         labs(title = "Histogram of Daily Steps (w/ imputed val)", x = "Steps per Day", y = "Frequency")
 # mean and median of imputed dataset
-mean(agg_date_imp.df$Steps)
-median(agg_date_imp.df$Steps)
+mean_imp <- mean(agg_date_imp.df$Steps)
+median_imp <- median(agg_date_imp.df$Steps)
+print(mean_imp)
+print(median_imp)
 
 # add DayType (weekend/weekday) feature into the imputed dataset
 weekend <- c("Saturday","Sunday")
@@ -54,15 +66,10 @@ imp.df$DayType <- as.factor(ifelse(weekdays(imp.df$date) %in% weekend, "Weekend"
 # aggregate data points over intervals
 agg_int_imp.df <- setNames(aggregate(imp.df$steps ~ imp.df$interval + imp.df$DayType, data = imp.df, FUN = mean), c("Interval","DayType", "Steps"))
 
-# panel plot for Interval Mean Steps - Weekday Vs. Weekend
+# time series panel plot for Interval Mean Steps - Weekday Vs. Weekend
 ggplot(agg_int_imp.df, aes(x = Interval, y = Steps, color = DayType)) +
         geom_line() +
         facet_wrap(~DayType, ncol = 1, nrow = 2) +
         labs(title = "Interval Mean Steps - Weekday Vs. Weekend", x = "Interval", y = "Mean Steps")
-
-
-
-
-
 
 
